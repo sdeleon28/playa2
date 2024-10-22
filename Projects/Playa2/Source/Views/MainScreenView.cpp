4781c7d463a2f7113c9cc4604bd29567f11bd184
Santiago de Leon <sdeleon28@gmail.com>
2024-10-22 19:04:52 -0300

    improve ux

--- Projects/Playa2/Source/Views/MainScreenView.cpp
+++ Projects/Playa2/Source/Views/MainScreenView.cpp
@@ -20,10 +20,27 @@ void MainScreenView::run() {
     return vbox({gauge(progress), separator(), menu->Render()}) | border;
   });
 
+  std::vector<char> nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
   out = CatchEvent(renderer, [&](Event event) {
+    if (event == Event::Character('g')) {
+      setCurrentEntry(0);
+      return true;
+    }
+    if (event == Event::Character('G')) {
+      setCurrentEntry((int)entries.size() - 1);
+      return true;
+    }
     if (event == Event::Character(' ')) {
       togglePlay();
       return true;
+    }
+    int i = 0;
+    for (auto c : nums) {
+      if (event == Event::Character(c) && (int)entries.size() > i) {
+        setCurrentEntry(i);
+        return true;
+      }
+      i++;
     }
     if (event == Event::Custom) {
       // Re-render the UI here
@@ -52,6 +69,14 @@ void MainScreenView::update(const IAppModel::DTO& dto) {
     progress = dto.progress;
     // After updating progress, trigger a UI update
     screen.PostEvent(Event::Custom);
+  }
+  if (dto.currentEntry.path != entries[(size_t)selected]) {
+    for (size_t i = 0; i < entries.size(); ++i) {
+      if (entries[i] == dto.currentEntry.path) {
+        selected = (int)i;
+        break;
+      }
+    }
   }
 }
 
